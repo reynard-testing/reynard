83d306c9fdd45787e0514093fe63ef48fe229796
Delano Flipse <delanoflipse@hotmail.com>
2025-03-10 15:43:19 +0100

    chore: revert random 8 space indentation

--- lib/src/test/java/nl/dflipse/fit/AppTest.java
+++ lib/src/test/java/nl/dflipse/fit/AppTest.java
@@ -23,75 +23,75 @@ import nl.dflipse.fit.instrument.services.InstrumentedService;
 @SuppressWarnings("resource")
 @Testcontainers(parallel = true)
 public class AppTest {
-        public static final InstrumentedApp app = new InstrumentedApp().withJaeger();
-        private static final String BASE_IMAGE = "go-micro-service:latest";
+    public static final InstrumentedApp app = new InstrumentedApp().withJaeger();
+    private static final String BASE_IMAGE = "go-micro-service:latest";
 
-        @Container
-        private static final InstrumentedService geo = app.instrument("geo", 8080,
-                        new GenericContainer<>(BASE_IMAGE)
-                                        .withCommand("go-micro-services geo"))
-                        .withHttp2();
+    @Container
+    private static final InstrumentedService geo = app.instrument("geo", 8080,
+            new GenericContainer<>(BASE_IMAGE)
+                    .withCommand("go-micro-services geo"))
+            .withHttp2();
 
-        @Container
-        private static final InstrumentedService rate = app.instrument("rate", 8080,
-                        new GenericContainer<>(BASE_IMAGE)
-                                        .withCommand("go-micro-services rate"))
-                        .withHttp2();
+    @Container
+    private static final InstrumentedService rate = app.instrument("rate", 8080,
+            new GenericContainer<>(BASE_IMAGE)
+                    .withCommand("go-micro-services rate"))
+            .withHttp2();
 
-        @Container
-        private static final InstrumentedService search = app.instrument("search", 8080,
-                        new GenericContainer<>(BASE_IMAGE)
-                                        .withCommand("go-micro-services search"))
-                        .withHttp2();
+    @Container
+    private static final InstrumentedService search = app.instrument("search", 8080,
+            new GenericContainer<>(BASE_IMAGE)
+                    .withCommand("go-micro-services search"))
+            .withHttp2();
 
-        @Container
-        private static final InstrumentedService profile = app.instrument("profile", 8080,
-                        new GenericContainer<>(BASE_IMAGE)
-                                        .withCommand("go-micro-services profile"))
-                        .withHttp2();
+    @Container
+    private static final InstrumentedService profile = app.instrument("profile", 8080,
+            new GenericContainer<>(BASE_IMAGE)
+                    .withCommand("go-micro-services profile"))
+            .withHttp2();
 
-        @Container
-        private static final GenericContainer<?> frontend = new GenericContainer<>(BASE_IMAGE)
-                        .withNetwork(app.network)
-                        .withCommand("go-micro-services frontend")
-                        .withExposedPorts(8080)
-                        .dependsOn(search.getService(), profile.getService());
+    @Container
+    private static final GenericContainer<?> frontend = new GenericContainer<>(BASE_IMAGE)
+            .withNetwork(app.network)
+            .withCommand("go-micro-services frontend")
+            .withExposedPorts(8080)
+            .dependsOn(search.getService(), profile.getService());
 
-        public static FaultController getController() {
-                return app;
-        }
+    public static FaultController getController() {
+        return app;
+    }
 
-        @BeforeAll
-        public static void setupServices() {
-                app.start();
-        }
+    @BeforeAll
+    public static void setupServices() {
+        app.start();
+    }
 
-        @AfterAll
-        static public void teardownServices() {
-                app.stop();
-        }
+    @AfterAll
+    static public void teardownServices() {
+        app.stop();
+    }
 
-        @FiTest
-        public void testApp(Faultload faultload) throws IOException {
-                int frontendPort = frontend.getMappedPort(8080);
-                String queryUrl = "http://localhost:" + frontendPort + "/hotels?inDate=2015-04-09&outDate=2015-04-10";
+    @FiTest
+    public void testApp(Faultload faultload) throws IOException {
+        int frontendPort = frontend.getMappedPort(8080);
+        String queryUrl = "http://localhost:" + frontendPort + "/hotels?inDate=2015-04-09&outDate=2015-04-10";
 
-                Response res = Request.get(queryUrl)
-                                .addHeader("traceparent", faultload.getTraceParent().toString())
-                                .addHeader("tracestate", faultload.getTraceState().toString())
-                                .execute();
+        Response res = Request.get(queryUrl)
+                .addHeader("traceparent", faultload.getTraceParent().toString())
+                .addHeader("tracestate", faultload.getTraceState().toString())
+                .execute();
 
-                String inspectUrl = app.orchestratorInspectUrl + "/v1/trace/" + faultload.getTraceId();
-                String traceUrl = "http://localhost:" + app.jaeger.getMappedPort(app.jaegerPort) + "/trace/"
-                                + faultload.getTraceId();
+        String inspectUrl = app.orchestratorInspectUrl + "/v1/trace/" + faultload.getTraceId();
+        String traceUrl = "http://localhost:" + app.jaeger.getMappedPort(app.jaegerPort) + "/trace/"
+                + faultload.getTraceId();
 
-                boolean containsError = faultload.getFaults().stream()
-                                .anyMatch(f -> f.getMode().getType().equals("HTTP_ERROR"));
-                int expectedResponse = containsError ? 500 : 200;
-                int actualResponse = res.returnResponse().getCode();
-                assertEquals(expectedResponse, actualResponse);
+        boolean containsError = faultload.getFaults().stream()
+                .anyMatch(f -> f.getMode().getType().equals("HTTP_ERROR"));
+        int expectedResponse = containsError ? 500 : 200;
+        int actualResponse = res.returnResponse().getCode();
+        assertEquals(expectedResponse, actualResponse);
 
-                boolean allRunning = app.allRunning();
-                assertTrue(allRunning);
-        }
+        boolean allRunning = app.allRunning();
+        assertTrue(allRunning);
+    }
 }

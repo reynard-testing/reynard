0c68fa54b7f0c59197049d00093f67dcac9d2b25
Delano Flipse <delanoflipse@hotmail.com>
2025-03-10 21:58:59 +0100

    chore: apply changes

--- lib/src/main/java/nl/dflipse/fit/strategy/TrackedFaultload.java
+++ lib/src/main/java/nl/dflipse/fit/strategy/TrackedFaultload.java
@@ -51,7 +51,7 @@ public class TrackedFaultload {
     }
 
     public String serializeJson() {
-        return TestCaseSerializer.serializeJson(this);
+        return TrackedFaultloadSerializer.serializeJson(this);
     }
 
     public String getTraceId() {

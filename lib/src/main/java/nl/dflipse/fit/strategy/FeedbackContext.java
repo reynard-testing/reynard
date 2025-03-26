4443b286decbddd965ab215912b5fba66653c136
Delano Flipse <delanoflipse@hotmail.com>
2025-03-26 15:08:02 +0100

    chore: store per context

--- lib/src/main/java/nl/dflipse/fit/strategy/FeedbackContext.java
+++ lib/src/main/java/nl/dflipse/fit/strategy/FeedbackContext.java
@@ -1,23 +1,33 @@
 package nl.dflipse.fit.strategy;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import nl.dflipse.fit.faultload.Fault;
 import nl.dflipse.fit.faultload.FaultUid;
 import nl.dflipse.fit.faultload.Faultload;
 import nl.dflipse.fit.faultload.faultmodes.FaultMode;
+import nl.dflipse.fit.strategy.store.DynamicAnalysisStore;
 import nl.dflipse.fit.strategy.util.Sets;
 
 public class FeedbackContext {
 
     private final String contextName;
     private final StrategyRunner runner;
+    private final DynamicAnalysisStore store;
+
+    private final static Map<String, DynamicAnalysisStore> stores = new HashMap<>();
 
     public FeedbackContext(StrategyRunner runner, String contextName) {
+        assertGeneratorPresent();
         this.contextName = contextName;
         this.runner = runner;
+
+        // restore store if it exists
+        this.store = stores.computeIfAbsent(contextName,
+                k -> new DynamicAnalysisStore(runner.getGenerator().getFaultModes()));
     }
 
     private void assertGeneratorPresent() {
@@ -27,39 +37,32 @@ public class FeedbackContext {
     }
 
     public Set<FaultMode> getFaultModes() {
-        assertGeneratorPresent();
         return runner.getGenerator().getFaultModes();
     }
 
     public Set<FaultUid> getFaultUids() {
-        assertGeneratorPresent();
         return runner.getGenerator().getFaultInjectionPoints();
     }
 
     public void reportFaultUids(List<FaultUid> faultInjectionPoints) {
-        assertGeneratorPresent();
         runner.getGenerator().reportFaultUids(faultInjectionPoints);
     }
 
     public void reportConditionalFaultUid(Set<Fault> subset, FaultUid fid) {
-        assertGeneratorPresent();
         runner.getGenerator().reportConditionalFaultUid(subset, fid);
     }
 
     public void pruneFaultUidSubset(Set<FaultUid> subset) {
-        assertGeneratorPresent();
         long reduction = runner.getGenerator().pruneFaultUidSubset(subset);
         runner.statistics.incrementEstimatePruner(contextName, reduction);
     }
 
     public void pruneFaultSubset(Set<Fault> subset) {
-        assertGeneratorPresent();
         long reduction = runner.getGenerator().pruneFaultSubset(subset);
         runner.statistics.incrementEstimatePruner(contextName, reduction);
     }
 
     public void pruneMixed(Set<Fault> subset, FaultUid fault) {
-        assertGeneratorPresent();
         long sum = 0;
         for (var mode : runner.getGenerator().getFaultModes()) {
             sum += runner.getGenerator().pruneFaultSubset(Sets.plus(subset, new Fault(fault, mode)));
@@ -68,7 +71,6 @@ public class FeedbackContext {
     }
 
     public void pruneFaultload(Faultload fautload) {
-        assertGeneratorPresent();
         long reduction = runner.getGenerator().pruneFaultload(fautload);
         runner.statistics.incrementEstimatePruner(contextName, reduction);
     }

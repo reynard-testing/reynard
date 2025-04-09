6e4028f81f3c127b9a0699ce94bfa643852030f9
Delano Flipse <delanoflipse@hotmail.com>
2025-04-09 14:50:13 +0200

    chore: fix indentation issues

--- lib/src/test/java/nl/dflipse/fit/suites/OTELSuiteIT.java
+++ lib/src/test/java/nl/dflipse/fit/suites/OTELSuiteIT.java
@@ -37,164 +37,162 @@ import okhttp3.Response;
  * FI test the app
  */
 public class OTELSuiteIT {
-        private static final RemoteController controller = new RemoteController("http://localhost:5000");
+    private static final RemoteController controller = new RemoteController("http://localhost:5000");
 
-        private static final int PORT = 8080;
-        private static final String USER_ID = "6894131c-cab3-4dfe-a5f1-a7086b9f7376";
-        private static final String CURRENCY = "USD";
-        private static final ObjectMapper mapper = new ObjectMapper();
+    private static final int PORT = 8080;
+    private static final String USER_ID = "6894131c-cab3-4dfe-a5f1-a7086b9f7376";
+    private static final String CURRENCY = "USD";
+    private static final ObjectMapper mapper = new ObjectMapper();
 
-        private OkHttpClient client = new OkHttpClient.Builder()
-                        .connectTimeout(5, TimeUnit.SECONDS)
-                        .readTimeout(10, TimeUnit.SECONDS)
-                        .build();
-        public static final MediaType JSON = MediaType.get("application/json");
+    private OkHttpClient client = new OkHttpClient.Builder()
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .build();
+    public static final MediaType JSON = MediaType.get("application/json");
 
-        public static FaultController getController() {
-                return controller;
+    public static FaultController getController() {
+        return controller;
+    }
+
+    private void addItemToCart() throws IOException {
+        var obj = mapper.createObjectNode();
+        var itemNode = mapper.createObjectNode();
+        itemNode.put("productId", "0PUK6V6EV0");
+        itemNode.put("quantity", 1);
+        obj.set("item", itemNode);
+        obj.put("userId", USER_ID);
+
+        String endpoint = "http://localhost:" + PORT + "/api/cart?currencyCode=" + CURRENCY;
+
+        RequestBody body = RequestBody.create(mapper.writeValueAsBytes(obj), JSON);
+        Request request = new Request.Builder()
+                // .addHeader("Content-Type", "application/json")
+                .url(endpoint)
+                .post(body)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            return;
         }
+    }
 
-        private void addItemToCart() throws IOException {
-                var obj = mapper.createObjectNode();
-                var itemNode = mapper.createObjectNode();
-                itemNode.put("productId", "0PUK6V6EV0");
-                itemNode.put("quantity", 1);
-                obj.set("item", itemNode);
-                obj.put("userId", USER_ID);
+    @FiTest(maskPayload = true)
+    public void testShipping(TrackedFaultload faultload) throws IOException {
 
-                String endpoint = "http://localhost:" + PORT + "/api/cart?currencyCode=" + CURRENCY;
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host("localhost")
+                .port(PORT)
+                .addPathSegments("api/shipping")
+                .addQueryParameter("itemList",
+                        "[{\"productId\":\"66VCHSJNUP\",\"quantity\":1}]")
+                .addQueryParameter("currencyCode", CURRENCY)
+                .addQueryParameter("address",
+                        "{\"streetAddress\":\"1600+Amphitheatre+Parkway\",\"city\":\"Mountain+View\",\"state\":\"CA\",\"country\":\"United+States\",\"zipCode\":\"94043\"}")
+                .build();
 
-                RequestBody body = RequestBody.create(mapper.writeValueAsBytes(obj), JSON);
-                Request request = new Request.Builder()
-                                // .addHeader("Content-Type", "application/json")
-                                .url(endpoint)
-                                .post(body)
-                                .build();
+        var traceparent = faultload.getTraceParent().toString();
+        var tracestate = faultload.getTraceState().toString();
 
-                try (Response response = client.newCall(request).execute()) {
-                        return;
-                }
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("traceparent", traceparent)
+                .addHeader("tracestate", tracestate)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            String inspectUrl = controller.apiHost + "/v1/trace/" + faultload.getTraceId();
+            String traceUrl = "http://localhost:16686/trace/" + faultload.getTraceId();
+
+            boolean containsError = faultload.hasFaultMode(ErrorFault.FAULT_TYPE, OmissionFault.FAULT_TYPE);
+            int expectedResponse = containsError ? 500 : 200;
+            int actualResponse = response.code();
+            assertEquals(expectedResponse, actualResponse);
         }
+    }
 
-        @FiTest(maskPayload = true)
-        public void testShipping(TrackedFaultload faultload) throws IOException {
+    @FiTest(maskPayload = true)
+    public void testRecommendations(TrackedFaultload faultload)
+            throws IOException {
 
-                HttpUrl url = new HttpUrl.Builder()
-                                .scheme("http")
-                                .host("localhost")
-                                .port(PORT)
-                                .addPathSegments("api/shipping")
-                                .addQueryParameter("itemList",
-                                                "[{\"productId\":\"66VCHSJNUP\",\"quantity\":1}]")
-                                .addQueryParameter("currencyCode", CURRENCY)
-                                .addQueryParameter("address",
-                                                "{\"streetAddress\":\"1600+Amphitheatre+Parkway\",\"city\":\"Mountain+View\",\"state\":\"CA\",\"country\":\"United+States\",\"zipCode\":\"94043\"}")
-                                .build();
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host("localhost")
+                .port(PORT)
+                .addPathSegments("api/recommendations")
+                .addQueryParameter("productIds", "")
+                .addQueryParameter("currencyCode", CURRENCY)
+                .build();
 
-                var traceparent = faultload.getTraceParent().toString();
-                var tracestate = faultload.getTraceState().toString();
+        var traceparent = faultload.getTraceParent().toString();
+        var tracestate = faultload.getTraceState().toString();
 
-                Request request = new Request.Builder()
-                                .url(url)
-                                .addHeader("traceparent", traceparent)
-                                .addHeader("tracestate", tracestate)
-                                .build();
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("traceparent", traceparent)
+                .addHeader("tracestate", tracestate)
+                .build();
 
-                try (Response response = client.newCall(request).execute()) {
-                        String inspectUrl = controller.apiHost + "/v1/trace/" + faultload.getTraceId();
-                        String traceUrl = "http://localhost:16686/trace/" + faultload.getTraceId();
+        try (Response response = client.newCall(request).execute()) {
+            String inspectUrl = controller.apiHost + "/v1/trace/" + faultload.getTraceId();
+            String traceUrl = "http://localhost:16686/trace/" + faultload.getTraceId();
 
-                        boolean containsError = faultload.hasFaultMode(ErrorFault.FAULT_TYPE, OmissionFault.FAULT_TYPE);
-                        int expectedResponse = containsError ? 500 : 200;
-                        int actualResponse = response.code();
-                        assertEquals(expectedResponse, actualResponse);
-                }
+            TraceAnalysis result = getController().getTrace(faultload);
+            boolean injectedFaults = !result.getInjectedFaults().isEmpty();
+
+            if (injectedFaults) {
+                assert (response.code() >= 500);
+                assert (response.code() < 600);
+            } else {
+                assertEquals(200, response.code());
+            }
         }
+    }
 
-        @FiTest(maskPayload = true)
-        public void testRecommendations(TrackedFaultload faultload)
-                        throws IOException {
+    @FiTest(maskPayload = true)
+    public void testCheckout(TrackedFaultload faultload) throws IOException {
+        addItemToCart();
 
-                HttpUrl url = new HttpUrl.Builder()
-                                .scheme("http")
-                                .host("localhost")
-                                .port(PORT)
-                                .addPathSegments("api/recommendations")
-                                .addQueryParameter("productIds", "")
-                                .addQueryParameter("currencyCode", CURRENCY)
-                                .build();
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("http")
+                .host("localhost")
+                .port(PORT)
+                .addPathSegments("api/checkout")
+                .addQueryParameter("productIds", "")
+                .addQueryParameter("currencyCode", CURRENCY)
+                .build();
 
-                var traceparent = faultload.getTraceParent().toString();
-                var tracestate = faultload.getTraceState().toString();
+        String payload = "{\"userId\":\"" + USER_ID
+                + "\",\"email\":\"someone@example.com\",\"address\":{\"streetAddress\":\"1600 Amphitheatre Parkway\",\"state\":\"CA\",\"country\":\"United States\",\"city\":\"Mountain View\",\"zipCode\":\"94043\"},\"userCurrency\":\""
+                + CURRENCY
+                + "\",\"creditCard\":{\"creditCardCvv\":672,\"creditCardExpirationMonth\":1,\"creditCardExpirationYear\":2030,\"creditCardNumber\":\"4432-8015-6152-0454\"}}";
+        RequestBody body = RequestBody.create(payload, JSON);
 
-                Request request = new Request.Builder()
-                                .url(url)
-                                .addHeader("traceparent", traceparent)
-                                .addHeader("tracestate", tracestate)
-                                .build();
+        var traceparent = faultload.getTraceParent().toString();
+        var tracestate = faultload.getTraceState().toString();
 
-                try (Response response = client.newCall(request).execute()) {
-                        String inspectUrl = controller.apiHost + "/v1/trace/" + faultload.getTraceId();
-                        String traceUrl = "http://localhost:16686/trace/" + faultload.getTraceId();
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .addHeader("traceparent", traceparent)
+                .addHeader("tracestate", tracestate)
+                .build();
 
-                        TraceAnalysis result = getController().getTrace(faultload);
-                        boolean injectedFaults = !result.getInjectedFaults().isEmpty();
+        try (Response response = client.newCall(request).execute()) {
+            String inspectUrl = controller.apiHost + "/v1/trace/" + faultload.getTraceId();
+            String traceUrl = "http://localhost:16686/trace/" + faultload.getTraceId();
 
-                        if (injectedFaults) {
-                                assert (response.code() >= 500);
-                                assert (response.code() < 600);
-                        } else {
-                                assertEquals(200, response.code());
-                        }
-                }
-        }
+            var traceData = controller.getTrace(faultload);
 
-        @FiTest(maskPayload = true)
-        public void testCheckout(TrackedFaultload faultload) throws IOException {
-                addItemToCart();
+            boolean containsError = traceData.hasFaultMode(ErrorFault.FAULT_TYPE,
+                    OmissionFault.FAULT_TYPE);
+            int actualResponse = response.code();
 
-                HttpUrl url = new HttpUrl.Builder()
-                                .scheme("http")
-                                .host("localhost")
-                                .port(PORT)
-                                .addPathSegments("api/checkout")
-                                .addQueryParameter("productIds", "")
-                                .addQueryParameter("currencyCode", CURRENCY)
-                                .build();
-
-                String payload = "{\"userId\":\"" + USER_ID
-                                + "\",\"email\":\"someone@example.com\",\"address\":{\"streetAddress\":\"1600 Amphitheatre Parkway\",\"state\":\"CA\",\"country\":\"United States\",\"city\":\"Mountain View\",\"zipCode\":\"94043\"},\"userCurrency\":\""
-                                + CURRENCY
-                                + "\",\"creditCard\":{\"creditCardCvv\":672,\"creditCardExpirationMonth\":1,\"creditCardExpirationYear\":2030,\"creditCardNumber\":\"4432-8015-6152-0454\"}}";
-                RequestBody body = RequestBody.create(payload, JSON);
-
-                var traceparent = faultload.getTraceParent().toString();
-                var tracestate = faultload.getTraceState().toString();
-
-                Request request = new Request.Builder()
-                                .url(url)
-                                .post(body)
-                                .addHeader("traceparent", traceparent)
-                                .addHeader("tracestate", tracestate)
-                                .build();
-
-                try (Response response = client.newCall(request).execute()) {
-                        String inspectUrl = controller.apiHost + "/v1/trace/" + faultload.getTraceId();
-                        String traceUrl = "http://localhost:16686/trace/" + faultload.getTraceId();
-
-                        var traceData = controller.getTrace(faultload);
-
-                        boolean containsError = traceData.hasFaultMode(ErrorFault.FAULT_TYPE,
-                                        OmissionFault.FAULT_TYPE);
-                        int actualResponse = response.code();
-
-                        // if (containsError) {
-                        // assertTrue(actualResponse >= 500 && actualResponse < 600);
-                        // } else {
-                        // assertEquals(200, actualResponse);
-                        // }
-                }
-
+            // if (containsError) {
+            // assertTrue(actualResponse >= 500 && actualResponse < 600);
+            // } else {
+            // assertEquals(200, actualResponse);
+            // }
         }
 
     }

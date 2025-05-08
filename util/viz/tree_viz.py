1c95649d6b6c32c7f469fe7713b147f5fb329ec1
Delano Flipse <delanoflipse@hotmail.com>
2025-05-09 00:24:45 +0200

    feat: combine nodes for clearer overview

--- util/viz/tree_viz.py
+++ util/viz/tree_viz.py
@@ -15,12 +15,14 @@ def get_args():
 
 
 def simplify_name(name: str):
-    no_fault = name.lstrip("Fault")
+    no_fault = name.replace("Fault", "")
     no_brackets = no_fault.replace("[", "").replace("]", "")
     if no_brackets == "":
-        return "[]"
+        return "[]", ""
 
     no_uid = no_brackets.replace("uid=", "")
+    if ", mode=" not in no_uid:
+        return no_uid, ""
     uid, mode = no_uid.split(", mode=")
 
     uid_parts = uid.split(">")
@@ -45,18 +47,54 @@ def simplify_name(name: str):
     mode = mode.replace("HTTP_ERROR(", "")
     mode = mode.replace(")", "")
 
-    return uid_str + "\n" + mode
+    return uid_str, mode
+
+
+COMBINE = True
 
 
 def build_tree(dot, node, parent=None):
-    simpl = simplify_name(node['node'])
+    uid, mode = simplify_name(node['node'])
     index = node['index']
-    dot.node(str(index), label=simpl)
+    dot.node(str(index), label=f"{uid}\n{mode}")
 
-    for child in node.get('children', []):
-        build_tree(dot, child, node)
-        dot.edge(str(node['index']), str(
-            child['index']), label=str(child['index']))
+    if COMBINE:
+        by_uid = {}
+        modes_by_uid = {}
+        for child in node.get('children', []):
+            child_uid, child_mode = simplify_name(child['node'])
+            if child_uid not in by_uid:
+                by_uid[child_uid] = []
+                modes_by_uid[child_uid] = []
+            by_uid[child_uid].append(child)
+            modes_by_uid[child_uid].append(child_mode)
+
+        for child_uid, children in by_uid.items():
+            if len(children) > 1:
+                child_mode = "Multiple"
+            else:
+                child_mode = modes_by_uid[child_uid][0]
+            combined_children = []
+            for child in children:
+                if 'children' in child:
+                    combined_children += child['children']
+
+            combined_index = ",".join([str(c['index']) for c in children])
+            min_index = min([c['index'] for c in children])
+            combined_index_label = f"{min_index} (+{len(children)})"
+            new_child = {
+                'node': child_uid,
+                'index': combined_index,
+                'children': combined_children
+            }
+            build_tree(dot, new_child, node)
+            dot.edge(str(node['index']), combined_index,
+                     label=combined_index_label)
+    else:
+        for child in node.get('children', []):
+            build_tree(dot, child, node)
+            dot.edge(str(node['index']), str(child['index']),
+                     label=str(child['index'] + 1))
 
 
 if __name__ == '__main__':

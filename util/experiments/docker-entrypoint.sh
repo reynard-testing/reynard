556c3770b3f17114db99ddeff048067f46890230
Delano Flipse <delanoflipse@hotmail.com>
2025-12-28 17:37:51 +0100

    chore: progress, but running into roadblocker

--- util/experiments/docker-entrypoint.sh
+++ util/experiments/docker-entrypoint.sh
@@ -22,22 +22,28 @@ done
 echo "Docker daemon is ready!"
 
 # 3. Build and install Reynard
-make build-all && make install
+make build-all
 
 # 4. Run the experiments
-echo "reynard.output.dir=results/tests" > lib/src/test/resources/junit-platform.properties
-N=${repeat_count} ./util/experiments/filibuster/run_all_filibuster_n.sh ${tag}
-N=${repeat_count} ./util/experiments/otel/run_all_otel.sh ${tag}
-N=${repeat_count} ./util/experiments/hotelreservation/run_all_n.sh ${tag}
-PROXY_RETRY_COUNT=0 N=${repeat_count} ./util/experiments/meta/run_all_meta.sh ${tag}
-PROXY_RETRY_COUNT=2 N=${repeat_count} ./util/experiments/meta/run_all_meta.sh ${tag}
-PROXY_RETRY_COUNT=4 N=${repeat_count} ./util/experiments/meta/run_all_meta.sh ${tag}
-N=${repeat_count} ./util/experiments/meta/run_all_meta.sh ${tag}
+echo "reynard.output.dir=/results/tests" > lib/src/test/resources/junit-platform.properties
+# N=${repeat_count} ./util/experiments/filibuster/run_all_filibuster_n.sh ${tag}
+# N=${repeat_count} ./util/experiments/otel/run_all_otel.sh ${tag}
+# N=${repeat_count} ./util/experiments/hotelreservation/run_all_n.sh ${tag}
+# PROXY_RETRY_COUNT=0 N=${repeat_count} ./util/experiments/meta/run_all_meta.sh ${tag}
+# PROXY_RETRY_COUNT=2 N=${repeat_count} ./util/experiments/meta/run_all_meta.sh ${tag}
+# PROXY_RETRY_COUNT=4 N=${repeat_count} ./util/experiments/meta/run_all_meta.sh ${tag}
+# N=${repeat_count} ./util/experiments/meta/run_all_meta.sh ${tag}
 
 # 4. Run the experiments without SER for comparison
-echo "reynard.output.dir=results/without-ser" > lib/src/test/resources/junit-platform.properties
-USER_SER=false N=1 ./util/experiments/filibuster/run_all_filibuster_n.sh ${tag}
-USER_SER=false N=1 ./util/experiments/otel/run_all_otel.sh ${tag}
-USER_SER=false N=1 ./util/experiments/hotelreservation/run_all_n.sh ${tag}
-USER_SER=false N=1 ./util/experiments/meta/run_all_meta.sh ${tag}
-USER_SER=false N=1 ./util/experiments/meta/run_all_meta.sh ${tag}
+# 4.1 Save results in a different directory
+echo "reynard.output.dir=/results/without-ser" > lib/src/test/resources/junit-platform.properties
+ser_tag="NO-SER${tag:+-$tag}"
+./util/experiments/otel/run_full_otel.sh shipping
+# Pause forever to keep the container running
+tail -f /dev/null
+
+# USER_SER=false N=1 ./util/experiments/filibuster/run_all_filibuster_n.sh ${ser_tag}
+# USER_SER=false N=1 ./util/experiments/otel/run_all_otel.sh ${ser_tag}
+# USER_SER=false N=1 ./util/experiments/hotelreservation/run_all_n.sh ${ser_tag}
+# USER_SER=false N=1 ./util/experiments/meta/run_all_meta.sh ${ser_tag}
+# USER_SER=false N=1 ./util/experiments/meta/run_all_meta.sh ${ser_tag}
